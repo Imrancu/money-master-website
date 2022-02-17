8f81396e37fb7fec23439fef9f15c2ad94a11c00
imrancu <mdimran3772@gmail.com>
2022-02-17 10:06:46 +0600

    ui Design done

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// const calculationButton = document.getElementById('calculation-btn');
+// calculationButton.addEventListener('click', function() {
+//     const totalIncome = document.getElementById('total-income');
+//     const foodCost = document.getElementById('food-cost').value;
+//     const rentalCost = document.getElementById('rental-cost').value;
+//     const otherCost = document.getElementById('other-cost').value;
+//     const expenses = document.getElementById('expenses');
+//     const balance = document.getElementById('balance');
+//     const totalCost = parseFloat(foodCost) + parseFloat(rentalCost) + parseFloat(otherCost);
+//     expenses.innerText = totalCost;
+// })
