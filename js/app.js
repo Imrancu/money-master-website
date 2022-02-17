1dbf967797f95f70c45388f772b62a8091ab9b43
imrancu <mdimran3772@gmail.com>
2022-02-17 10:21:22 +0600

    added a little bit code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
+// Income & Cost Calculation
 const calculationButton = document.getElementById('calculation-btn');
 calculationButton.addEventListener('click', function() {
-    const totalIncome = document.getElementById('total-income').value;
-    const foodCost = document.getElementById('food-cost').value;
-    const rentalCost = document.getElementById('rental-cost').value;
-    const otherCost = document.getElementById('other-cost').value;
-    const expenses = document.getElementById('expenses');
-    const balance = document.getElementById('balance');
-    const totalCost = parseFloat(foodCost) + parseFloat(rentalCost) + parseFloat(otherCost);
-    expenses.innerText = totalCost;
-    balance.innerText = parseFloat(totalIncome) - totalCost;
-})
+        const totalIncome = document.getElementById('total-income').value;
+        const foodCost = document.getElementById('food-cost').value;
+        const rentalCost = document.getElementById('rental-cost').value;
+        const otherCost = document.getElementById('other-cost').value;
+        const expenses = document.getElementById('expenses');
+        const balance = document.getElementById('balance');
+        const totalCost = parseFloat(foodCost) + parseFloat(rentalCost) + parseFloat(otherCost);
+        expenses.innerText = totalCost;
+        balance.innerText = parseFloat(totalIncome) - totalCost;
+    })
+    // Saving calculation
