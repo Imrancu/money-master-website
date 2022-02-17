c7593cb21d4d55836c7510a0b1fb822ce4068236
imrancu <mdimran3772@gmail.com>
2022-02-17 10:11:06 +0600

    half done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-// const calculationButton = document.getElementById('calculation-btn');
-// calculationButton.addEventListener('click', function() {
-//     const totalIncome = document.getElementById('total-income');
-//     const foodCost = document.getElementById('food-cost').value;
-//     const rentalCost = document.getElementById('rental-cost').value;
-//     const otherCost = document.getElementById('other-cost').value;
-//     const expenses = document.getElementById('expenses');
-//     const balance = document.getElementById('balance');
-//     const totalCost = parseFloat(foodCost) + parseFloat(rentalCost) + parseFloat(otherCost);
-//     expenses.innerText = totalCost;
-// })
+const calculationButton = document.getElementById('calculation-btn');
+calculationButton.addEventListener('click', function() {
+    const totalIncome = document.getElementById('total-income').value;
+    const foodCost = document.getElementById('food-cost').value;
+    const rentalCost = document.getElementById('rental-cost').value;
+    const otherCost = document.getElementById('other-cost').value;
+    const expenses = document.getElementById('expenses');
+    const balance = document.getElementById('balance');
+    const totalCost = parseFloat(foodCost) + parseFloat(rentalCost) + parseFloat(otherCost);
+    expenses.innerText = totalCost;
+    balance.innerText = parseFloat(totalIncome) - totalCost;
+})
